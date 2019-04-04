7a205d390fd7279235816264b8e3ca142e89460d
Emanuel Aracena <emanuel.aracena57@myhunter.cuny.edu>
2019-04-04 00:37:52 -0400

    Updated baseline-1 to full functionality.
    Modifications include:
    * Uses a global variable to avoid recomputing permutation set every time agent is called.
    * Now successfully removes already guessed combinations from code set.
    * Every time it is called the agent gives the next guess in list, thereby conforming to game rules.
    * Folding issues were fixed and now can fold sets of various sizes.

--- baseline-1.lisp
+++ baseline-1.lisp
@@ -6,12 +6,18 @@
 ;; The agent will need some form of memory
 (defvar *previous-guess* nil)
 
+;; Avoid recomputing same set every function call
+(defvar *all-possible-codes* nil)
+
 ;; Function performs the special condition final fold needed to create a usable possibility set.
 ;; Essentially map folded segment over final subset
 (defun final-fold (set)
+  (if (listp (first set))
   (loop for subset in set
      append (loop for item in (member-if #'listp subset)
-	   collect (append (remove-if #'listp subset) item))))
+	       collect (append (remove-if #'listp subset) item)))
+  (loop for item in (member-if #'listp set)
+     collect (append (remove-if #'listp set) item))))
 
 ;; Generate a set of subsets of all possible combinations, result requires folding to be
 ;; considered usable. Functions is recursive/iterative.
@@ -32,33 +38,41 @@
 ;;             LS  = NIL
 ;;             ACC = ((A A (A B) (B B) (C B)) (A B (A B) (B B) (C B)))
 (defun fold (ls)
+  (declare (notinline fold))
   (labels
       ((aux (x ls acc)
   	 ;(break)
   	 (cond
-  	   ((endp ls) acc)
+	   ((endp ls) acc)
 	   (T (aux x (rest ls) (append acc (list (cons  x (first ls)))))))))
     (aux (first ls) (rest ls) '())))
 
 
 
 (defun baseline-1-MoonlightPinkFlamingoes (board colors SCSA last-response)
-  (declare (ignore SCSA))
+  (declare (ignore SCSA last-response))
   (let (possible)
-    ;; Permuate a subset for every possible combination
-    (setf possible (permutate colors board))
+    ;; If the set of all possible codes hasn't been generated, generate only once and store in global variable
+    (if (null *all-possible-codes*)
+	;; Permuate a subset for every possible combination
+	(progn
+	  (setf possible (permutate colors board))
+	  ;; Recursively fold subsets until one last level remains (special condition: final-fold)
+	  (setf possible (loop for item in possible collect (fold item)))
+	  ;; Perform final fold and possible will be left with all possible permutations
+	  (loop for x from 1 to (- board 2)
+	     do (setf possible (loop for item in possible append (final-fold item))))
+	  (setf *all-possible-codes* possible)))
 
-    ;; Recursively fold subsets until one last level remains (special condition: final-fold)
-    (setf possible (loop for item in possible collect (fold item)))
-
-    ;; Perform final fold and possible will be left with all possible permutations
-    (setf possible (loop for item in possible append (final-fold item)))
-
+    ;; Remember previously generated list
+    (setf possible *all-possible-codes*)
+    
     ;; Remove already used guesses from the front of list
-    (setf possible (remove last-response (member *previous-guess* possible)))
-
+    (if (not (null (member *previous-guess* *all-possible-codes*)))
+	(setf possible (rest (member *previous-guess* *all-possible-codes*))))
+    
     ;; Store guess in gobal variable so it retains the information out of scope
     (setf *previous-guess* (first possible))
-    
+    (print *previous-guess*)
     ;; finally return next guess in lexographical order:
     (first possible)))
