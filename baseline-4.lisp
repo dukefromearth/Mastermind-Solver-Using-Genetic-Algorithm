50a62ffa11f618681a8a1a9a5b2bd5caf79cdeb8
Emanuel Aracena <emanuel.aracena57@myhunter.cuny.edu>
2019-04-06 00:45:22 -0400

    Implemented a few helper functions.
    Added working implementations to the Getnext helper functions: tied, itscolor, and nextpos.

--- baseline-4.lisp
+++ baseline-4.lisp
@@ -4,38 +4,51 @@
 ;;               The comment explanations of functions "Update" and "GetNext", as well as their helper functions,
 ;;               are from "AN ALGORITHM TO PLAY THE GAME OF MASTERMIND" by T. Magadeva Rao.
 
-;; Keeps track of inferences out of scope
+;; Keeps track of inferences
 (defvar *inferences*)
 
+;; Previous guess
+(defvar *trial*)
+
 ;; Getnext Algorithm constructs the next trial arrangement
-;; Helpers:
-
-;;   Length(L): integer; Returns the length, the number of top-level elements in a list L
-
-;;   Secondunfixed(inferences): colors; Returns the second color which is not yet fixed.
 (defun getnext (trial)
   (loop for i in trial
        (cond ((tied i) (setf i (itscolor i)))
-	     ((= i (nextpos beingfixed)) (setf i 'beingfixed))
-	     ((= (length *inferences*) (length trial)) (setf i (secondunfixed(*inferences*))))
-	     (t (setf i 'beingconsidered)))))
+ 	     ((= i (nextpos beingfixed)) (setf i 'beingfixed))
+ 	     ((= (length *inferences*) (length trial)) (setf i (secondunfixed *inferences*)))
+ 	     (t (setf i 'beingconsidered)))))
+
 ;;;;
 ;;;; GETNEXT helpers
 ;;;;
+;;   Tied(pos): boolean; Returns true if the position pos has a color tied to it
+;;   Example:
+;;     Inference list = ((C1 (P1 P2)) (C2 (P2 P3))
+;;     (tied 4) => false, not color is tied to position 4
+(defun tied (pos)
+  (loop for sublist in *inferences*
+       ;; Loop through inferences until found, otherwise nill
+       thereis (not (null (member pos (second sublist))))))
 
-;;   Tied(i): boolean; Returns true if the position i has a color tied to it
-(defun tied ())
-
-;;   Itscolor(i): colors; When i is a tied position returns the color to which i is tied
-(defun itscolor ())
+;;   Itscolor(pos) : colors; When pos is a tied position returns the color to which i is tied
+(defun itscolor (pos)
+  (loop for sublist in *inferences*
+     ;; Loop through inferences until found, otherwise nill
+     when (not (null (member pos (second sublist))))
+     ;; Return color
+     return (first sublist)))
 
 ;;   Nextpos(i): positions; Returns the next possible position for color i. That is if i = 3
 ;;                          and the corresponding sublist is (3 (2 4 5)) then it returns 2.
-(defun nextpos ())
-
-;;
-;; Secondunfixed (inferences) : colors, Returns the second color which is not yet fixed
-(defun secondunfixed (inferences))
+(defun nextpos (color)
+  (loop for sublist in *inferences*
+     when (equal (first sublist) color)
+     return (first (second sublist))))
+       
+;; Secondunfixed : colors, Returns the second color which is not yet fixed
+(defun secondunfixed ()
+  ;; Uses global *inferences* as argument
+  )
 
 ;;;;
 ;;;; UPDATE algorithm updates knowledge base
@@ -51,7 +64,7 @@
     (1 (progn
 	 (if () ; beingfixed <> 0
 	     (del beingfixed beingconsidered))
-       (del beingfixed beingconsidered)))
+	 (del beingfixed beingconsidered)))
     (2 (fix1 beingconsidered beingfixed))
     (otherwise (report-error-update)))
   (cleanup *inferences*)
@@ -67,7 +80,7 @@
 
 ;;   Addlists(gain, beingconsidered, inferences): Adds sublists (equal in number to gain) to the list inferences,
 ;;                                                each sublist with header 'being considered'
-(defun addlists (gain beingconsidered inferences))
+(defun addlists ())
 
 ;;   Fix (beingfixed): 'Fix'es the beingfixed in its next possibleposition and deletes appropriate position from
 ;;                      other lists. For example, if beingfixed = 3, and inferences is:
@@ -78,21 +91,21 @@
 ;;                                              ((3 (2))
 ;;                                               (4 (3 4 5))
 ;;                                               (7 (1 3 4 5))
-(defun fix (beingfixed))
+(defun fix ())
 
 ;;   Bump(beingfixed): BeingFixed will get an updated value. In the above example, beingfixed will become 4.
-(defun bump (beingfixed))
+(defun bump ())
 
 ;;   Del(i j): Deletes the current position of the color i from the sublist for color j. For example, if i=2, j=3
 ;;             and the sublists are
 ;;             (2 (3 4 5))
 ;;             (3 (2 3 4 5))
-(defun del (i j))
+(defun del ())
 
 ;;   Fix1(i j); fixes the color i in the current position of the color j. In the above example, if i=3 and j=2,
 ;;              then the color 3 gets tied to Position 3, its sublist becomes (3 (3)), and the positon 3 gets
 ;;              deleted from the other sublists
-(defun fix1(i j))
+(defun fix1 ())
 
 ;;   Cleanup(inferences); cleans up the inferences list. For example, if inferences was:
 ;;                        ((2 (3))
@@ -106,24 +119,24 @@
 ;;                        ((2 (3))
 ;;                         (4 (4))
 ;;                         (5 (5))
-(defun cleanup (inferences))
+;(defun cleanup (inferences))
 
 ;;   Nextcolor(beingconsidered): Gets a new color for being considered. if all the five colors have already
 ;;                               been detected then being considered is simple set to zero
-(defun nextcolor (beingconsidered))
+(defun nextcolor ())
 
 ;; Main routine function
 (defun baseline-4-MoonlightPinkFlamingoes (board color SCSA last-response)
   ;; Update (inferences)
   ;; Getnext (trial)
   ;; Numfix (inferences): Numfix simply returns the number, in this case the nubmer of inferences
-
+  
   ;; Update the inferences
   (update *inferences*)
-
+  
   ;; Get next guess
   (getnext trial)
-
+  
   ;; return new guess
   trial)
 
