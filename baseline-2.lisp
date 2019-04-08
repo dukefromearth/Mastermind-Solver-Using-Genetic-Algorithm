bee7ff41eb9c8a11f405cb4cca71dd3f0b7efd5b
Stephen Duke <stephen_duke@live.com>
2019-04-07 20:28:50 -0400

    Initial Commit
    remove_if_any_match: deletes any element that contains item 1 at position 1, item 2 at position 2

--- baseline-2.lisp
+++ baseline-2.lisp
@@ -15,7 +15,7 @@
   (if (listp (first set))
   (loop for subset in set
      append (loop for item in (member-if #'listp subset)
-	       collect (append (remove-if #'listp subset) item)))
+         collect (append (remove-if #'listp subset) item)))
   (loop for item in (member-if #'listp set)
      collect (append (remove-if #'listp set) item))))
 
@@ -24,10 +24,10 @@
 (defun permutate (set length)
   (if (= length 0)
       (loop for item in set
-	 collect item))
+   collect item))
   (if (> length 0)
       (loop for item in set
-	 collect (append (list item) (permutate set (- length 1))))))
+   collect (append (list item) (permutate set (- length 1))))))
 
 ;; Merge first element with every sublist
 ;; Original list: '(A (A (A B) (B B) (C B)) (B (A B) (B B) (C B))))
@@ -41,28 +41,43 @@
   ;(declare (notinline fold))
   (labels
       ((aux (x ls acc)
-  	 ;(break)
-  	 (cond
-	   ((endp ls) acc)
-	   (T (aux x (rest ls) (append acc (list (cons  x (first ls)))))))))
+     ;(break)
+     (cond
+     ((endp ls) acc)
+     (T (aux x (rest ls) (append acc (list (cons  x (first ls)))))))))
     (aux (first ls) (rest ls) '())))
 
+;; Deletes any element that contains item 1 at position 1, item 2 at position 2
+;; For example
+;; (prune-sequence '(A B) '((B C) (B A) (A B) (A A)))
+;; ((B C) (B A)
+(defun remove_if_any_match (sequence ls)
+  (labels
+      ((aux (sequence ls acc)
+	 (cond
+	   ((endp ls) acc)
+	   (T (loop for item in sequence
+		    for x from 0
+		    if (equal item (nth x (first ls)))
+		 do (setf (first ls) nil))
+	      (if (endp (first ls)) (aux sequence (rest ls) acc)
+			(aux sequence (rest ls) (reverse (cons (first ls) acc))))))))
+    (aux sequence ls '())))
 
-
-(defun baseline-1-MoonlightPinkFlamingoes (board colors SCSA last-response)
+(defun baseline-2-MoonlightPinkFlamingoes (board colors SCSA last-response)
   (declare (ignore SCSA last-response))
   (let (possible)
     ;; If the set of all possible codes hasn't been generated, generate only once and store in global variable
     (if (null *all-possible-codes*)
-	;; Permuate a subset for every possible combination
-	(progn
-	  (setf possible (permutate colors board))
-	  ;; Recursively fold subsets until one last level remains (special condition: final-fold)
-	  (setf possible (loop for item in possible collect (fold item)))
-	  ;; Perform final fold and possible will be left with all possible permutations
-	  (loop for x from 1 to (- board 2)
-	     do (setf possible (loop for item in possible append (final-fold item))))
-	  (setf *all-possible-codes* possible)))
+  ;; Permuate a subset for every possible combination
+  (progn
+    (setf possible (permutate colors board))
+    ;; Recursively fold subsets until one last level remains (special condition: final-fold)
+    (setf possible (loop for item in possible collect (fold item)))
+    ;; Perform final fold and possible will be left with all possible permutations
+    (loop for x from 1 to (- board 2)
+       do (setf possible (loop for item in possible append (final-fold item))))
+    (setf *all-possible-codes* possible)))
 
     ;; Remember previously generated list
     (setf possible *all-possible-codes*)
@@ -70,10 +85,10 @@
     ;; Remove already used guesses from the front of list
 
     (if (not (null (member *previous-guess* *all-possible-codes*)))
-	      (setf possible (rest (member *previous-guess* *all-possible-codes*))))
+        (setf possible (rest (member *previous-guess* *all-possible-codes*))))
 
     ;; Store guess in gobal variable so it retains the information out of scope
     (setf *previous-guess* (first possible))
     ;(print *previous-guess*)
     ;; finally return next guess in lexographical order:
-    (first possible)))
+(first possible)))
