0be4183b039f779f0c91ae082be36a3e3cc555df
Ajani Stewart <theamjaha@gmail.com>
2019-04-14 20:22:04 -0400

    baseline 2 now works

--- baseline-2.lisp
+++ baseline-2.lisp
@@ -24,27 +24,13 @@
     (aux (first ls) (rest ls) '())))
 
 (defun final-fold (set)
+  (if (listp (first set))
   (loop for subset in set
      append (loop for item in (member-if #'listp subset)
-	       collect (append (remove-if #'listp subset) item))))
+	       collect (append (remove-if #'listp subset) item)))
+  (loop for item in (member-if #'listp set)
+collect (append (remove-if #'listp set) item))))
 
-
-;; Takes a sequence (A A B) and removes any guess that contains any
-;; For example
-;; (prune-sequence '(A B) '((B C) (B A) (A B) (A A)))
-;; ((B C) (B A)
-(defun prune-sequence (sequence ls)
- (labels
-   ((aux (sequence ls acc)
-	 (cond
-	  ((endp ls) acc)
-	  (T (loop for item in sequence
-		  for x from 0
-		  if (equal item (nth x (first ls)))
-		 do (setf (first ls) nil))
-	   (if (endp (first ls)) (aux sequence (rest ls) acc)
-			(aux sequence (rest ls) (reverse (cons (first ls) acc))))))))
-   (aux sequence ls '())))
 
 (defun unique-items (sequence)
   (let (unique)
@@ -59,7 +45,10 @@
     ;; copying permuation code from baseline 1
     (setf output (permutate set length))
     (setf output (loop for item in output collect (fold item)))
-    (setf output (loop for item in output append (final-fold item)))))
+    (loop for x from 1 to (- length 2)
+       do (setf output (loop for item in output append (final-fold item)))
+       finally
+	 (return output))))
 
 ;; memory is cheap but time is expensive
 (defvar *previous-responses* nil)
@@ -70,23 +59,29 @@
 (defvar *permutations* nil)
 (defvar *possible-colors* nil)
 
+;;print all variables and inspect
+
 (defun baseline-2-MoonlightPinkFlamingoes (board colors SCSA last-response)
+  (declare (ignore SCSA))
   (cons last-response *previous-responses*)
   ;; set up permutations
+  ;(format t "1")
   (if (endp *permutations*)
       (progn
-	(setf *permutations* (generate-permutations (colors board)))
+	(setf *permutations* (generate-permutations colors board))
 	(setf *possible-colors* colors)))
+  ;(format t "2")
   (cond
     ;; On first turn
-    ((endp last-response) ((cons (first *permutations*) *previous-guesses*)
+    ((endp last-response) (progn (cons (first *permutations*) *previous-guesses*)
 			   (setf *last-guess* (first *permutations*))
-			   (setf *permutations* (rest *premutations))))
+			   (setf *permutations* (rest *permutations*))))
     ;; if last response was (0 0)
     ;; remove those colors from the list of candidates and permute again
     ;; removing the guesses that we have already made
     ((and (= (nth 0 last-response) 0) (= (nth 1 last-response) 0))
-     
+
+    ; (format t "3")
      (setf *possible-colors* (set-difference *possible-colors* *last-guess*))
      (setf *permutations* (generate-permutations *possible-colors* board))
      (setf *permutations* (set-difference *permutations* *previous-guesses*))
@@ -96,6 +91,7 @@
 
     ;; if last response was (x board) then the answer is some permutation of the last guess
     ((= (nth 1 last-response) board)
+     ;(format t "4")
      (setf *possible-colors* (unique-items *last-guess*))
      (setf *permutations* (generate-permutations *possible-colors* board))
      (cons (first *permutations*) *previous-guesses*)
@@ -103,9 +99,16 @@
      (setf *permutations* (rest *permutations*)))
     
     ;; else just guess the next permutation in the sequence
-    (t  (cons (first *permutations*) *previous-guesses*)
-	(setf *last-guess* (first *permutations*))
-	(setf *permutations* (rest *permutations*))))
+    
+    (t
+     ;(format t "5")
+     (cons (first *permutations*) *previous-guesses*)
+     (setf *last-guess* (first *permutations*))
+     (setf *permutations* (rest *permutations*))))
+  ;(format t "previous-responses: ~a~%" *previous-responses*)
+  ;(format t "last-guess: ~a~%" *last-guess*)
+  ;(format t "previous-guesses: ~a~%" *previous-guesses*)
+  ;(format t "possible-colors: ~a~%" *possible-colors*)
 
   *last-guess*)
      
