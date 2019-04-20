dba3803e89abce0b4df40b81338b5d76de9c4001
Emanuel Aracena <emanuel.aracena57@myhunter.cuny.edu>
2019-04-20 13:48:55 -0400

    Implemented additional metrics.
    Benchmark now lists the winning guess sequence if round was won and the number of guesses taken for wins or losses.

--- benchmark.lisp
+++ benchmark.lisp
@@ -14,7 +14,7 @@
 ;;     Ex: (Mastermind 4 6 'ab-color)
 ;;
 ;;   Ensure that you save previous benchmark data by changing its name, IT WILL GET OVERWRITTEN
-;;   once you run this program (Change "benchmark-statistics.txt" to anything else).
+;;   you run this program (Change "benchmark-statistics.txt" to anything else).
 ;;
 ;;   Run this benchmark using Mastermind parameters
 ;;     Ex: (benchmark 'baseline-3-MoonlightPinkFlamingoes 'ab-color 10)
@@ -22,20 +22,23 @@
 ;;   Check directory for benchmark data file.
 
 (defun benchmark (agent-name SCSA number-of-rounds)
-  (let (real1 real2 run1 run2)
+  (let (real1 real2 run1 run2 result)
     (with-open-file (stream "benchmark-statistics.txt"
 			    :direction :output     ;; write to file
 			    :if-exists :supersede  ;; overwrite file if file associated with filename exits
 			    :if-does-not-exist :create)
       (format t "~%Benchmark ran with agent [~a] with SCSA [~a] for [~a] rounds:~%~%" agent-name SCSA number-of-rounds)
-      (format stream "Benchmark ran with agent [~a] with SCSA [~a] for [~a] rounds:~%" agent-name SCSA number-of-rounds)
+      (format stream "~%Benchmark ran with agent [~a] with SCSA [~a] for [~a] rounds:~%~%" agent-name SCSA number-of-rounds)
       (loop for i from 1 to number-of-rounds
 	 ;; Get timings
 	 do (setf real1 (get-internal-real-time))
 	 do (setf run1 (get-internal-real-time))
 
 	 ;; Run tournament round
-	 do (format stream "Round ~a: ~a~%" i (play-tournament *Mastermind* agent-name SCSA 1))
+	 do (format stream "Round ~a: ~a~%" i (setf result (play-tournament *Mastermind*
+									    agent-name
+									    SCSA
+									    1)))
 
 	 ;; Get second timings
 	 do (setf run2 (get-internal-real-time))
@@ -45,6 +48,18 @@
 	 do (format stream "Timing:~%")
 	 do (format stream "  Round took:~%")
 	 do (format stream "      ~f seconds of real time~%" (/ (- real2 real1) internal-time-units-per-second))
-	 do (format stream "      ~f seconds of run time~%~%" (/ (- run2 run1) internal-time-units-per-second))))))
+	 do (format stream "      ~f seconds of run time~%~%" (/ (- run2 run1) internal-time-units-per-second))
+
+	 ;;;; Analysis
+	 ;; Number of guesses
+	 do (format stream "Number of guesses made: ~a~%~%" (length *guesses*))
+
+	 ;; If win, post winning sequence
+	 if (> 0 (first result))
+	 do (progn
+	      (format stream "~%Winning sequence:~%")
+	      (loop for i in *guesses*
+		 do (format stream "~a~%" i)))))))
+	       
   
 
