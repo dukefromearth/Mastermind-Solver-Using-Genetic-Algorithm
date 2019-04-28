5068305d664545c81d7dbf131e6a55f3a6e0807e
Emanuel Aracena <emanuel.aracena57@myhunter.cuny.edu>
2019-04-28 18:52:59 -0400

    Added "average number of guesses" metric.

--- benchmark.lisp
+++ benchmark.lisp
@@ -23,9 +23,10 @@
 
 (defun benchmark (agent-name SCSA number-of-rounds)
   (let ((rounds-won 0)
-	(average-score 0)
-	(average-real-time 0)
-	(average-run-time 0)
+	(total-score 0)
+	(total-real-time 0)
+	(total-run-time 0)
+	(total-number-of-guesses 0)
 	real1 real2 run1 run2 result)
     (with-open-file (stream "benchmark-statistics.txt"
 			    :direction :output     ;; write to file
@@ -42,7 +43,7 @@
 	 do (format stream "~%Round ~a: ~a~%" i (setf result (play-tournament *Mastermind* agent-name SCSA 1)))
 
 	 ;; Add to average
-	 do (setf average-score (+ average-score (first result)))
+	 do (setf total-score (+ total-score (first result)))
 	   
 	 ;; Get second timings
 	 do (setf run2 (get-internal-real-time))
@@ -54,11 +55,13 @@
 	 do (format stream "      ~f seconds of real time~%" (/ (- real2 real1) internal-time-units-per-second))
 	 do (format stream "      ~f seconds of run time~%~%" (/ (- run2 run1) internal-time-units-per-second))
 
-	 ;; Collect averages
-	 do (setf average-real-time (+ average-real-time (/ (- real2 real1) internal-time-units-per-second)))
-	 do (setf average-run-time (+ average-run-time (/ (- run2 run1) internal-time-units-per-second)))
+	 ;; Collect totals
+	 do (setf total-real-time (+ total-real-time
+				     (/ (- real2 real1) internal-time-units-per-second)))
+	 do (setf total-run-time (+ total-run-time
+				    (/ (- run2 run1) internal-time-units-per-second)))
 	   
-	   
+	 do (setf total-number-of-guesses (+ total-number-of-guesses (length *guesses*)))
 	 ;;;; Analysis
 	 ;; Number of guesses
 	 do (format stream "Number of guesses made: ~a~%" (length *guesses*))
@@ -72,12 +75,13 @@
 	      (setf rounds-won (+ rounds-won 1))))
 
       ;; Print post tournament metrics
-      (format stream "~%Post-tournament metrics:~%  Average score: ~a ~%" (float (/ average-score number-of-rounds)))
-      (format stream "  Average real time: ~a~%" (float (/ average-real-time number-of-rounds)))
-      (format stream "  Average run time: ~a~%" (float (/ average-run-time number-of-rounds)))
+      (format stream "~%Post-tournament metrics:~%  Average score: ~a ~%" (float (/ total-score number-of-rounds)))
+      (format stream "  Average real time: ~a~%" (float (/ total-real-time number-of-rounds)))
+      (format stream "  Average run time: ~a~%" (float (/ total-run-time number-of-rounds)))
+      (format stream "  Average number of guesses: ~a~%" (float (/ total-number-of-guesses number-of-rounds)))
       (format stream "  Rounds won: ~a~%" rounds-won)
       (format stream "  Rounds lost: ~a~%" (- number-of-rounds rounds-won))
-      (format stream "  Win rate: ~a~%" (/ rounds-won number-of-rounds)))))
+      (format stream "  Win rate: ~a~%" (float (/ rounds-won number-of-rounds))))))
 	       
   
 
